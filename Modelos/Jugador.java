e61023a4002d30bcd81a330c69cfcf572a8bb1d8
RmFelipe24 <rimunozro@est.utn.ac.cr>
2024-11-03 21:13:12 -0600

    Clase jugador
    Se creo la clase jugador con sus get, set y constructor de color y nombre

--- Modelos/Jugador.java
+++ Modelos/Jugador.java
@@ -9,5 +9,39 @@ package Modelos;
  * @author Crisp
  */
 public class Jugador {
-    
+
+    private String nombre;
+    private int color;
+    public String jugador1;
+    public String jugador2;
+    public int jugadorActual;
+    public String ultimoGanador;
+
+    public Jugador() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public String getJugador1() {
+        return jugador1;
+    }
+
+    public String getJugador2() {
+        return jugador2;
+    }
+
+    public int getJugadorActual() {
+        return jugadorActual;
+    }
+
+    public Jugador(String nombre, int color) {
+        this.nombre = nombre;
+        this.color = color;
+    }
 }
