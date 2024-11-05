4de4ac23ac39e0884e70802361bcc4852b5f196b
RmFelipe24 <rimunozro@est.utn.ac.cr>
2024-11-04 23:10:12 -0600

    Clase FichaBoot
    Se creo sus atributos de color blanco y negro

---
+++ Modelos/FichaBoot.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Modelos;
+
+/**
+ *
+ * @author munoz
+ */
+public class FichaBoot {
+    public int negro = 1;
+    public int blanco = 2;
+    
+    public FichaBoot() {
+    }
+
+    public int getNegro() {
+        return negro;
+    }
+
+    public void setNegro(int negro) {
+        this.negro = negro;
+    }
+
+    public int getBlanco() {
+        return blanco;
+    }
+
+    public void setBlanco(int blanco) {
+        this.blanco = blanco;
+    }
+
+}
